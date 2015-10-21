60d6bbd83907524a3243d36c500216865f01e3f6
Andrei Lyskov <andreilyskov@gmail.com>
2015-10-20 20:22:41 -0700

    updating auth

--- server.py
+++ server.py
@@ -3,22 +3,48 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from utils.mongo_json_encoder import JSONEncoder
+import bcrypt
+from functools import wraps
+
 # Basic Setup
 app = Flask(__name__)
 mongo = MongoClient('localhost', 27017)
 app.db = mongo.develop_database
 api = Api(app)
+app.bcrypt_rounds = 12
 
 
-class User(Resource):
-    def post(self):
-        new_user = request.json
-        user_list = app.db.users
-        result = user_list.insert_one(new_user)
-        user = user_list.find_one({"_id": ObjectId(result.inserted_id)})
-        return user
+def check_auth(username, password):
+    user_list = app.db.users
+    user = user_list.find_one({"username": username})
+    if user is None:
+        return False
+    # the password that was stored in the database (currently hashed)
+    stored_pw = user["password"]
+    encoded_pw = password.encode("utf-8")
+    hashed_pw = bcrypt.hashpw(encoded_pw, bcrypt.gensalt(app.bcrypt_rounds))
+    print(stored_pw)
+    print(hashed_pw)
+    return stored_pw == hashed_pw
 
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            message = {'error': 'Basic Auth Required.'}
+            resp = jsonify(message)
+            resp.status_code = 401
+            return resp
+        return f(*args, **kwargs)
+    return decorated
+
+
+class Users(Resource):
+    @requires_auth
     def get(self, user_id):
+        print("is this working?")
         user_list = app.db.users
         user = user_list.find_one({"_id": ObjectId(user_id)})
         if user is None:
@@ -28,77 +54,79 @@ class User(Resource):
         else:
             return user
 
-api.add_resource(User, '/users/', '/users/<string:user_id>')
+    def post(self):
+        new_user = request.json
+        user_collection = app.db.users
+        encoded_pw = new_user["password"].encode('utf-8')
+        hashed_pw = bcrypt.hashpw(encoded_pw, bcrypt.gensalt(app.bcrypt_rounds))
+        new_user["password"] = hashed_pw
+        result = user_collection.insert_one(new_user)
+        # why do I need to return here?
+        inserted_user = user_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        del inserted_user['password']  # DO NOT return hashed password to client
+        print(inserted_user)
+        return inserted_user
 
 
-class Trip(Resource):
-    # creating a trip with the waypoints
+api.add_resource(Users, '/users/', '/users/<string:user_id>')
+
+
+class Trips(Resource):
+    @requires_auth
+    # get all functionality
+    # if I set trip_id to none, and I pass a value, will it change?
+    def get(self, trip_id=None):
+        # retrieve database of trips (resource)
+        trip_list = app.db.trips
+        if trip_id:
+            trip = trip_list.find_one({"_id": ObjectId(trip_id)})
+            # if none throw an error
+            if trip is None:
+                response = jsonify(data=[])
+                response.status_code = 404
+                return response
+            else:
+                return trip
+        else:
+            user_trips = trip_list.find_many({"username": request.authorization.username})
+            return user_trips
+
+    # posts a new trip with ID
+    @requires_auth
     def post(self):
         new_trip = request.json
-        trip_list = app.db.trips
-        result = trip_list.insert_one(new_trip)
-        trip = trip_list.find_one({"_id": ObjectId(result.inserted_id)})
-        return trip
+        new_trip["username"] = request.authorization.username
+        # Should I add a username/pw to this json request?
+        trip_collection = app.db.trips
+        result = trip_collection.insert_one(new_trip)
+        new_trip = trip_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        return new_trip
 
-    # retrieve a specific trip ID
-    def get(self, trip_id=None):
-        trip_list = app.db.trips
-        trip = trip_list.find_one({"_id": ObjectId(trip_id)})
-        if trip is None:
-            response = jsonify(data=[])
-            response.status_code = 404
-            return response
-        else:
-            return trip
-
-    # get all the trip ID's
-    def get_all(self):
-        trip_list = app.db.trips
-        if trip_list is None:
-            response = jsonify(data=[])
-            response.status_code = 404
-            return response
-        else:
-            return trip_list
-
-    # updating a trip with waypoints
+    # what does trip_id get input from?
+    @requires_auth
     def put(self, trip_id):
-        trip = trip_list.find_one({"_id": ObjectId(trip_id)})
-        if trip is None:
-            response = jsonify(data=[])
-            response.status_code = 404
-            return response
-        else:
-            modifying_trip = request.json
-            trip_list = app.db.trips
-            trip = trip_list.update_one({"_id": ObjectId(trip_id)})
-            return trip
+        new_trip = request.json
+        trip_collection = app.db.trips
+        old_trip = trip_collection.update_one({"_id": ObjectId(trip_id)}, {'$set': new_trip})
+        return old_trip
 
-    # deleting a specific waypoint
+    # Takes in trip_id, deletes, and returns response
+    @requires_auth
     def delete(self, trip_id):
         trip_collection = app.db.trips
-        trip = trip_collection.find_one({"_id": ObjectId(trip_id)})
-        if trip is None:
+        trip_collection.delete_one({"_id": ObjectId(trip_id)})
+        deleted_object = trip_collection.find_one({"_id": ObjectId(trip_id)})
+        # should return False since it deleted the object
+        if not deleted_object:
             response = jsonify(data=[])
-            response.status_code = 404
+            response.status_code = 200
             return response
         else:
-            if trip["username"] == request.json["username"]:
-                result = trip_collection.delete_one({"_id": ObjectId(trip_id)})
-                if result.deleted_count == 1:
-                    response = jsonify(data=[])
-                    response.status_code = 200
-                    return response
-                else:
-                    response = jsonify(data=[])
-                    response.status_code = 500
-                    return response
-            else:
-                response = jsonify(data=[])
-                response.status_code = 401
-                return response
+            response = jsonify(data=[])
+            response.status_code = 304
+            return response
 
-api.add_resource(Trip, '/trips/', '/trips/<string:trip_id>')
+api.add_resource(Trips, '/trips/', '/trips/<string:trip_id>')
 
 
 # provide a custom JSON serializer for flaks_restful
@@ -109,7 +137,6 @@ def output_json(data, code, headers=None):
     return resp
 
 if __name__ == '__main__':
-    # Turn this on in debug mode to get detailed information
-    # about request related exceptions: http://flask.pocoo.org/docs/0.10/config
+    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import server
 import unittest
 import json
 from pymongo import MongoClient
+from base64 import b64encode
 
 
 class FlaskrTestCase(unittest.TestCase):
@@ -17,41 +18,83 @@ class FlaskrTestCase(unittest.TestCase):
         server.app.db = db
 
         # Drop collection (significantly faster than dropping entire db)
-        db.drop_collection('users')
+        db.drop_collection('myobjects')
 
         # MyObject tests
 
-    def test_posting_myobject(self):
-        response = self.app.post(
-            '/users/',
-            data=json.dumps(dict(name="A object")),
+    def test_creating_user(self):
+        print("Creating user")
+        response = self.app.post('/users/',
+            data=json.dumps(dict(username="name", password="github")),
             content_type='application/json')
 
         responseJSON = json.loads(response.data.decode())
 
         self.assertEqual(response.status_code, 200)
         assert 'application/json' in response.content_type
-        assert 'A object' in responseJSON["name"]
+        assert 'name' in responseJSON["username"]
 
-    def test_getting_object(self):
-        print("Tests getting object")
-        response = self.app.post(
-            '/users/',
-            data=json.dumps(dict(name="Another object")),
-            content_type='application/json')
+    def test_getting_user(self):
+        print("Getting user")
+        # pass a user ID
+        username = "name"
+        password = "github"
+        headers = {'Authorization': 'Basic ' + b64encode("{0}:{1}".format(username, password).encode("utf-8")).decode("utf-8")}
+        response = self.app.get('/users/', headers=headers)
+        print(response)
+        # inserted_user = user_collection.find_one({"username": 'name'})
+        # print(user_collection)
 
-        postResponseJSON = json.loads(response.data.decode())
-        postedObjectID = postResponseJSON["_id"]
-
-        response = self.app.get('/users/' + postedObjectID)
-        responseJSON = json.loads(response.data.decode())
-
-        self.assertEqual(response.status_code, 200)
-        assert 'Another object' in responseJSON["name"]
-
-    def test_getting_non_existent_object(self):
-        response = self.app.get('/users/55f0cbb4236f44b7f0e3cb23')
-        self.assertEqual(response.status_code, 404)
+        # headers = {'Authorization': 'Basic ' + b64encode("{0}:{1}".format(username, password).encode("utf-8")).decode("utf-8")}
+        # response = self.app.get('/users/' + username, headers=headers)
+        #
+        # responseJSON = json.loads(response.data.decode())
+        #
+        # self.assertEqual(response.status_code, 200)
+        # assert 'application/json' in response.content_type
+        # assert 'name' in responseJSON["username"]
+        #
+    #
+    # def test_checking_auth(self):
+    #     break
+    #
+    # def test_posting_myobject(self):
+    #     response = self.app.post(
+    #         '/trips/',
+    #         data=json.dumps(dict(name="A object")),
+    #         content_type='application/json')
+    #
+    #     responseJSON = json.loads(response.data.decode())
+    #
+    #     self.assertEqual(response.status_code, 200)
+    #     assert 'application/json' in response.content_type
+    #     assert 'A object' in responseJSON["name"]
+    #
+    # def test_getting_object(self):
+    #     print("Tests getting object")
+    #     response = self.app.post(
+    #         '/trips/',
+    #         data=json.dumps(dict(name="Another object")),
+    #         content_type='application/json')
+    #
+    #     postResponseJSON = json.loads(response.data.decode())
+    #     postedObjectID = postResponseJSON["_id"]
+    #
+    #     response = self.app.get('/trips/' + postedObjectID)
+    #     responseJSON = json.loads(response.data.decode())
+    #
+    #     self.assertEqual(response.status_code, 200)
+    #     assert 'Another object' in responseJSON["name"]
+    #
+    # def test_getting_non_existent_object(self):
+    #     response = self.app.get('/trips/55f0cbb4236f44b7f0e3cb23')
+    #     self.assertEqual(response.status_code, 404)
+    #
+    # def test_modifying_object(self):
+    #     break
+    #
+    # def test_deleting_object(self):
+    #     break
 
 if __name__ == '__main__':
     unittest.main()
